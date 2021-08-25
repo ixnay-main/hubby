7f3f634a6fb411e089273654845baf5605547b91
Antony <allinto@icloud.com>
2021-08-25 21:12:58 +1200

    hubby

---
+++ src/nodejs_upload_file.js
@@ -0,0 +1,64 @@
+var http = require('http');
+var fs = require('fs');
+var formidable = require('formidable');
+
+const gun = require('gun')();
+const hub = require('gun/lib/hub'); 
+
+
+ 
+// html file containing upload form
+var upload_html = fs.readFileSync("upload_file.html");
+ 
+// replace this with the location to save uploaded files
+var upload_path = "/Users/yenan/Desktop/GitHub/Series01/test/temphome/";
+ 
+http.createServer(function (request, response) {
+    if (request.url == '/uploadform') {
+        response.writeHead(200);
+        response.write(upload_html);
+
+      return response.end();
+    } else if (request.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(request, function (err, fields, files) {
+            // oldpath : temporary folder to which file is saved to
+            var oldpath = files.filetoupload.path;
+
+            var newpath = upload_path + files.filetoupload.name;
+
+            var shortName = files.filetoupload.name
+            
+            console.log("gogogo")
+
+
+
+  
+
+
+
+            // copy the file to a new location
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+
+
+                console.log("go")
+    
+
+                // you may respond with another html page
+                response.write('File uploaded and moved!' + "you uploaded:" + shortName);
+       
+
+
+                hub.watch(`./temphome`);
+                gun.get("hub").on(data => {
+                    response.write(data[`/temphome/${shortName}`]) // Get the content of index.html
+                })
+                
+ 
+
+                response.end();
+            });
+        });
+    } 
+}).listen(8086);
---
+++ src/readMe.md
@@ -0,0 +1,10 @@
+to start use
+
+``` node nodejs_upload_file.js```
+
+issues will be caused by the radata folder, delete it and try again, or write some better code
+
+this repo takes a file upload of text, chucks it in temphome, then and allows for interaction with gun.
+
+it does not currently store this data under a gun hash, or allow for multiple file uploads
+
